582255d1f231723fa882243cd03d0b4e6dea1212
lColinDl <colin.daum@hotmail.de>
2021-04-06 16:03:16 +0200

    one lower case todo

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(), //TODO in here
+      home: HomePage(), //todo in here
     );
   }
 }
